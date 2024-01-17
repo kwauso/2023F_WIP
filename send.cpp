7d547e525dee8fda483e1fc12d52171949e0929b
otter <135718867+kwauso@users.noreply.github.com>
2024-01-18 01:47:43 +0900

    Update send.cpp

--- send.cpp
+++ send.cpp
@@ -1,6 +1,35 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main(){
-  cout << "Hello, World!" << endl;
-  return;
+int
+main()
+{
+ int sock;
+ struct sockaddr_in addr;
+ in_addr_t ipaddr;
+
+ sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+ addr.sin_family = AF_INET;
+ addr.sin_port = htons(12345);
+ addr.sin_addr.s_addr = inet_addr("224.0.0.1");
+
+ ipaddr = inet_addr("127.0.0.1");
+ if (setsockopt(sock,
+		IPPROTO_IP,
+		IP_MULTICAST_IF,
+		(char *)&ipaddr, sizeof(ipaddr)) != 0) {
+	perror("setsockopt");
+	return 1;
+ }
+
+ sendto(sock, "HELLO, World!", 13, 0, (struct sockaddr *)&addr, sizeof(addr));
+
+ close(sock);
+
+ return 0;
 }
