d88d5e303798acf692e8e470936ffc8c69bc3af8
otter <135718867+kwauso@users.noreply.github.com>
2024-01-25 08:11:01 +0900

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,47 @@
+import socket
+import datetime
+import sys
+
+#server: python s hostname.local port_number extension
+#
+
+
+def server(hostname, port, extention):
+    ip = socket.gethostbyname(str(hostname))
+    output_list = []
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ip, port))
+        s.listen(1)
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                #dt_now = datetime.datetime.now()
+                filename = extention
+                with open(filename, mode="ab") as f:
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        f.write(data)
+                        conn.sendall(b'Received done')
+                    exit()
+
+def client(hostname, port, filename):
+    ip = socket.gethostbyname(str(hostname))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, port))
+        try:
+            with open(filename, mode='rb') as f:
+                for line in f:
+                    s.sendall(line)
+                    data = s.recv(1024)
+                print(repr(data.decode()))
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    if (sys.argv[1] == "s"):
+        server((sys.argv[2]), int(sys.argv[3]), sys.argv[4])
+    if (sys.argv[1] == "c"):
+        client((sys.argv[2]), int(sys.argv[3]), sys.argv[4])
